9645c8458292e2c83cfea7f9fb06bf455ec893af
ronualdo <raxmac@gmail.com>
2014-04-05 12:11:59 -0300

    condigura a aplicacao para rodar utilizando o jetty

--- src/main/java/br/com/insubordinada/Application.java
+++ src/main/java/br/com/insubordinada/Application.java
@@ -1,8 +1,9 @@
-package br.com.insubordinada.controller;
+package br.com.insubordinada;
 
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+import spark.servlet.SparkApplication;
 import spark.template.freemarker.FreeMarkerRoute;
 
 import java.util.HashMap;
@@ -10,9 +11,9 @@ import java.util.Map;
 
 import static spark.Spark.get;
 
-public class Routes {
-
-    public static void main(String[] args) {
+public class Application implements SparkApplication {
+    @Override
+    public void init() {
         get(new FreeMarkerRoute("/hello") {
             @Override
             public ModelAndView handle(Request request, Response response) {
